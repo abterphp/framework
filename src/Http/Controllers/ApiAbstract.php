130950ee393f96311fd3b8768db0abb7752be214
Peter Aba <peter@peteraba.com>
2019-06-21 21:31:33 +0200

    Api base controller moved out of Admin

--- src/Http/Controllers/ApiAbstract.php
+++ src/Http/Controllers/ApiAbstract.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace AbterPhp\Framework\Http\Controllers\Admin;
+namespace AbterPhp\Framework\Http\Controllers;
 
 use AbterPhp\Framework\Databases\Queries\FoundRows;
 use AbterPhp\Framework\Domain\Entities\IStringerEntity;
@@ -38,18 +38,27 @@ abstract class ApiAbstract extends Controller
     /** @var FoundRows */
     protected $foundRows;
 
+    /** @var string */
+    protected $problemBaseUrl;
+
     /**
      * ApiAbstract constructor.
      *
      * @param LoggerInterface     $logger
      * @param RepoServiceAbstract $repoService
      * @param FoundRows           $foundRows
+     * @param string              $problemBaseUrl
      */
-    public function __construct(LoggerInterface $logger, RepoServiceAbstract $repoService, FoundRows $foundRows)
-    {
-        $this->logger      = $logger;
-        $this->repoService = $repoService;
-        $this->foundRows   = $foundRows;
+    public function __construct(
+        LoggerInterface $logger,
+        RepoServiceAbstract $repoService,
+        FoundRows $foundRows,
+        string $problemBaseUrl
+    ) {
+        $this->logger         = $logger;
+        $this->repoService    = $repoService;
+        $this->foundRows      = $foundRows;
+        $this->problemBaseUrl = $problemBaseUrl;
     }
 
     /**
@@ -98,17 +107,17 @@ abstract class ApiAbstract extends Controller
      */
     public function create(): Response
     {
-        $data = $this->getCreateData();
-
-        $errors = $this->repoService->validateForm($data);
-
-        if (count($errors) > 0) {
-            $msg = sprintf(static::LOG_MSG_CREATE_FAILURE, static::ENTITY_SINGULAR);
-
-            return $this->handleErrors($msg, $errors);
-        }
-
         try {
+            $data = $this->getCreateData();
+
+            $errors = $this->repoService->validateForm($data);
+
+            if (count($errors) > 0) {
+                $msg = sprintf(static::LOG_MSG_CREATE_FAILURE, static::ENTITY_SINGULAR);
+
+                return $this->handleErrors($msg, $errors);
+            }
+
             $fileData = $this->getFileData($data);
             $entity   = $this->repoService->create($data, $fileData);
         } catch (\Exception $e) {
@@ -127,17 +136,17 @@ abstract class ApiAbstract extends Controller
      */
     public function update(string $entityId): Response
     {
-        $data = $this->getUpdateData();
-
-        $errors = $this->repoService->validateForm($data);
-
-        if (count($errors) > 0) {
-            $msg = sprintf(static::LOG_MSG_UPDATE_FAILURE, static::ENTITY_SINGULAR, $entityId);
-
-            return $this->handleErrors($msg, $errors);
-        }
-
         try {
+            $data = $this->getUpdateData();
+
+            $errors = $this->repoService->validateForm($data);
+
+            if (count($errors) > 0) {
+                $msg = sprintf(static::LOG_MSG_UPDATE_FAILURE, static::ENTITY_SINGULAR, $entityId);
+
+                return $this->handleErrors($msg, $errors);
+            }
+
             $fileData = $this->getFileData($data);
             $entity   = $this->repoService->retrieveEntity($entityId);
             $this->repoService->update($entity, $data, $fileData);
@@ -237,9 +246,24 @@ abstract class ApiAbstract extends Controller
     {
         $this->logger->debug($msg);
 
+        $detail = [];
+        foreach ($errors as $key => $keyErrors) {
+            foreach ($keyErrors as $keyError) {
+                $detail[] = sprintf('%s: %s', $key, $keyError);
+            }
+        }
+
+        $status  = ResponseHeaders::HTTP_BAD_REQUEST;
+        $content = [
+            'type'   => sprintf('%sbad-request', $this->problemUrlBase),
+            'title'  => 'Bad Request',
+            'status' => $status,
+            'detail' => implode("\n", $detail),
+        ];
+
         $response = new Response();
-        $response->setStatusCode(ResponseHeaders::HTTP_BAD_REQUEST);
-        $response->setContent(json_encode(['errors' => $errors]));
+        $response->setStatusCode($status);
+        $response->setContent(json_encode($content));
 
         return $response;
     }
@@ -254,9 +278,17 @@ abstract class ApiAbstract extends Controller
     {
         $this->logger->error($msg, $this->getExceptionContext($exception));
 
+        $status  = ResponseHeaders::HTTP_INTERNAL_SERVER_ERROR;
+        $content = [
+            'type'   => sprintf('%sinternal-server-error', $this->problemUrlBase),
+            'title'  => 'Internal Server Error',
+            'status' => $status,
+            'detail' => $exception->getMessage(),
+        ];
+
         $response = new Response();
-        $response->setStatusCode(ResponseHeaders::HTTP_INTERNAL_SERVER_ERROR);
-        $response->setContent(json_encode(['errors' => [$msg]]));
+        $response->setStatusCode($status);
+        $response->setContent(json_encode($content));
 
         return $response;
     }
