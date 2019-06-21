932fc10ebdc68b19c32adb2d64af7093d60eab8c
Peter Aba <peter@peteraba.com>
2019-06-21 20:13:16 +0200

    Fixup

--- src/Http/Controllers/Admin/ApiAbstract.php
+++ src/Http/Controllers/Admin/ApiAbstract.php
@@ -156,7 +156,7 @@ abstract class ApiAbstract extends Controller
 
     /**
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     * 
+     *
      * @param array $data
      *
      * @return UploadedFile[]
