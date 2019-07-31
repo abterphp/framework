9d88e6afb0536c74937ebb37934467481480f9e7
Peter Aba <peter@peteraba.com>
2019-08-01 00:07:48 +0200

    Add phan

---
+++ .phan/config.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'target_php_version'              => '7.2',
+    'directory_list'                  => ['src/', 'vendor/'],
+    'exclude_analysis_directory_list' => ['vendor/'],
+    'quick_mode' => true,
+    'analyze_signature_compatibility' => true,
+    'minimum_severity' => 0,
+    'allow_missing_properties' => false,
+    'null_casts_as_any_type' => false,
+    'null_casts_as_array' => false,
+    'array_casts_as_null' => false,
+    'scalar_implicit_cast' => true, // TODO: Consider removing
+    'scalar_implicit_partial' => [],
+    'ignore_undeclared_variables_in_global_scope' => true, // TODO: No globals!
+    'suppress_issue_types' => [
+        // 'PhanUndeclaredMethod',
+    ],
+];
