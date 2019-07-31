feb2a331faeb1ad5e400d76489151144b3aa3cc5
Peter Aba <peter@peteraba.com>
2019-07-31 17:19:37 +0200

    Add tests for assets factory

--- src/Assets/Factory/Minifier.php
+++ src/Assets/Factory/Minifier.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AbterPhp\Framework\Assets\Factory;
 
 use MatthiasMullie\Minify\CSS as CssMinifier;
---
+++ tests/Assets/Factory/MinifierTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AbterPhp\Framework\Assets\Factory;
+
+use MatthiasMullie\Minify\CSS as CssMinifier;
+use MatthiasMullie\Minify\JS as JsMinifier;
+use PHPUnit\Framework\TestCase;
+
+class MinifierTest extends TestCase
+{
+    /** @var Minifier - System Under Test */
+    protected $sut;
+
+    public function setUp()
+    {
+        $this->sut = new Minifier();
+
+        parent::setUp();
+    }
+
+    public function testCreateCssMinifier()
+    {
+        $actualResult = $this->sut->createCssMinifier();
+
+        $this->assertInstanceOf(CssMinifier::class, $actualResult);
+    }
+
+    public function testCreateJsMinifier()
+    {
+        $actualResult = $this->sut->createJsMinifier();
+
+        $this->assertInstanceOf(JsMinifier::class, $actualResult);
+    }
+}
