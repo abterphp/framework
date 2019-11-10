7a33aa840a22ee6059dd51468644dbe7d31482e0
Peter Aba <peter@peteraba.com>
2019-11-10 23:43:36 +0100

    MigrationsBootstrapper simplification

--- src/Bootstrappers/Databases/MigrationsBootstrapper.php
+++ src/Bootstrappers/Databases/MigrationsBootstrapper.php
@@ -2,21 +2,17 @@
 
 namespace AbterPhp\Framework\Bootstrappers\Databases;
 
-use Opulence\Databases\IConnection;
-use Opulence\Databases\Migrations\FileMigrationFinder;
-use Opulence\Databases\Migrations\IMigrator;
-use Opulence\Databases\Migrations\Migrator;
 use Opulence\Framework\Configuration\Config;
 use Opulence\Framework\Databases\Bootstrappers\MigrationBootstrapper as OpulenceMigrationsBootstrapper;
-use Opulence\Framework\Databases\Migrations\ContainerMigrationResolver;
 use Opulence\Ioc\IContainer;
 
 class MigrationsBootstrapper extends OpulenceMigrationsBootstrapper
 {
+
     /**
-     * @param IContainer $container
+     * @inheritdoc
      */
-    protected function registerMigrator(IContainer $container)
+    public function registerBindings(IContainer $container)
     {
         global $abterModuleManager;
 
@@ -28,18 +24,8 @@ class MigrationsBootstrapper extends OpulenceMigrationsBootstrapper
 
         $paths = array_merge($globalPaths, $abterPaths);
 
-        $container->bindFactory(
-            IMigrator::class,
-            function () use ($container, $paths) {
-                $fileMigrationFinder = new FileMigrationFinder();
-                $resolver            = new ContainerMigrationResolver($container);
+        Config::set('paths', 'database.migrations', $paths);
 
-                $migrationPaths         = $fileMigrationFinder->findAll($paths);
-                $connection             = $container->resolve(IConnection::class);
-                $executedMigrationsRepo = $this->getExecutedMigrationRepository($container);
-
-                return new Migrator($migrationPaths, $connection, $resolver, $executedMigrationsRepo);
-            }
-        );
+        parent::registerBindings($container);
     }
 }
