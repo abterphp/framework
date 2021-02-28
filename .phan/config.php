1850280d1fd9163c251383da795e7c384ac66aaf
Peter Aba <peter@peteraba.com>
2021-02-28 15:03:50 +0100

    phan config code-style fixes

--- .phan/config.php
+++ .phan/config.php
@@ -3,20 +3,20 @@
 declare(strict_types=1);
 
 return [
-    'target_php_version'              => '7.4',
-    'directory_list'                  => ['src/', 'vendor/'],
-    'exclude_analysis_directory_list' => ['vendor/'],
-    'quick_mode' => true,
-    'analyze_signature_compatibility' => true,
-    'minimum_severity' => 0,
-    'allow_missing_properties' => false,
-    'null_casts_as_any_type' => false,
-    'null_casts_as_array' => false,
-    'array_casts_as_null' => false,
-    'scalar_implicit_cast' => true, // TODO: Consider removing
-    'scalar_implicit_partial' => [],
+    'target_php_version'                          => '7.4',
+    'directory_list'                              => ['src/', 'vendor/'],
+    'exclude_analysis_directory_list'             => ['vendor/'],
+    'quick_mode'                                  => true,
+    'analyze_signature_compatibility'             => true,
+    'minimum_severity'                            => 0,
+    'allow_missing_properties'                    => false,
+    'null_casts_as_any_type'                      => false,
+    'null_casts_as_array'                         => false,
+    'array_casts_as_null'                         => false,
+    'scalar_implicit_cast'                        => true, // TODO: Consider removing
+    'scalar_implicit_partial'                     => [],
     'ignore_undeclared_variables_in_global_scope' => true, // TODO: No globals!
-    'suppress_issue_types' => [
+    'suppress_issue_types'                        => [
         'PhanTypeInvalidThrowsIsInterface',
     ],
 ];
